b6b241f352e56373c876d26a278c86f158bf145b
anacolell <anacolell8@gmail.com>
2022-12-03 20:19:10 +0200

    Day 2 part 2

--- day2/index.js
+++ day2/index.js
@@ -5,6 +5,8 @@ const input = fs
   .split("\n")
   .map((line) => line.split(" "));
 
+//part 1
+
 const moves = {
   rock: 1,
   paper: 2,
@@ -24,17 +26,14 @@ let finalScore = 0;
 
 function wins(opponentMove, ourMove) {
   if (opponentMove === ourMove) {
-    console.log("tie");
     finalScore += 3 + ourMove;
   } else if (
     (opponentMove === moves.rock && ourMove === moves.paper) ||
     (opponentMove === moves.paper && ourMove === moves.scissors) ||
     (opponentMove === moves.scissors && ourMove === moves.rock)
   ) {
-    console.log("win");
     finalScore += 6 + ourMove;
   } else {
-    console.log("lose");
     finalScore += ourMove;
   }
   return finalScore;
@@ -43,6 +42,39 @@ function wins(opponentMove, ourMove) {
 for (const move of input) {
   let move1 = playerMoves[move[0]];
   let move2 = playerMoves[move[1]];
+  wins(move1, move2);
+}
+
+console.log(finalScore);
+
+//part 2
+
+const desiredOutcomes = {
+  //rock
+  A: {
+    X: moves.scissors,
+    Y: moves.rock,
+    Z: moves.paper,
+  },
+  //paper
+  B: {
+    X: moves.rock,
+    Y: moves.paper,
+    Z: moves.scissors,
+  },
+  //scissors
+  C: {
+    X: moves.paper,
+    Y: moves.scissors,
+    Z: moves.rock,
+  },
+};
+
+finalScore = 0;
+
+for (const move of input) {
+  let move1 = playerMoves[move[0]];
+  let move2 = desiredOutcomes[move[0]][move[1]];
   wins(move1, move2);
 }
 
